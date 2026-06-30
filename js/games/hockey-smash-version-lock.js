7ff2ed4fb6cbe4c93bd8b6eecc6b5a32eb6ca199
jtripppiie <48491097+jtripppiie@users.noreply.github.com>
2026-06-30 14:38:52 -0700

    Bump version for 20-salmon opening run

--- js/games/hockey-smash-version-lock.js
+++ js/games/hockey-smash-version-lock.js
@@ -1,6 +1,6 @@
 (function () {
-  const VERSION = 'Hockey Smash v0.14.45';
-  const BUILD = 'Build 2026-06-30.101';
+  const VERSION = 'Hockey Smash v0.14.46';
+  const BUILD = 'Build 2026-06-30.102';
   const LABEL = `${VERSION} · ${BUILD}`;
   let observerStarted = false;
 
